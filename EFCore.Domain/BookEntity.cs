621465113045d3bea19ca2baa1f4282985f26971
Artem Gilmanov <artemapochta@gmail.com>
2024-09-22 12:00:38 +0200

    Domain BookEntity and GenreEntity Id set to Guid.

--- EFCore.Domain/BookEntity.cs
+++ EFCore.Domain/BookEntity.cs
@@ -1,7 +1,7 @@
 ﻿namespace EFCore.Domain;
 public class BookEntity
 {
-  public int Id { get; set; }
+  public Guid Id { get; set; }
   public string Title { get; set; }
   public string Author { get; set; }
   public string Isbn { get; set; }
--- EFCore.Domain/GenreEntity.cs
+++ EFCore.Domain/GenreEntity.cs
@@ -2,7 +2,7 @@
 
 public class GenreEntity
 {
-  public int Id { get; set; }
+  public Guid Id { get; set; }
   public string Name { get; set; }
   public int Display { get; set; }
 }
