2d8562aee95476cd67a14a3088f14564593b0ec0
Artem Gilmanov <artemapochta@gmail.com>
2024-09-22 23:10:22 +0200

    Conntrollers created book and bookDetails controllers.

---
+++ EFCore/Controllers/BookController.cs
@@ -0,0 +1,181 @@
+﻿namespace EFCore.Api.Controllers;
+
+using EFCore.Domain;
+using EFCore.Resources;
+using Infrastructure.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class BookController : ControllerBase
+{
+  private readonly Repository db;
+
+  public BookController(Repository db)
+  {
+    this.db = db;
+  }
+
+  [HttpPost(Name = "Create")]
+  [ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public async Task<IActionResult> Create(Book resource)
+  {
+    var BookEntity = new BookEntity
+    {
+      Title = resource.Title,
+      Author = resource.Author,
+      Isbn = resource.Isbn,
+      Price = resource.Price,
+      PriceRange = resource.PriceRange,
+    };
+
+    await this.db.Books.AddAsync(BookEntity);
+    await this.db.SaveChangesAsync();
+
+    return this.Ok();
+  }
+
+  [HttpGet(Name = "GetAll")]
+  [ProducesResponseType(typeof(List<Book>), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public async Task<IActionResult> GetAll()
+  {
+    //List<BookEntity> bookEntities = await this.db.Books.ToListAsync();
+
+    //var books = bookEntities.Select(bookEntity => new Book
+    //{
+    //  Id = bookEntity.Id.ToString(),
+    //  Title = bookEntity.Title,
+    //  Author = bookEntity.Author,
+    //  Isbn = bookEntity.Isbn,
+    //  Price = bookEntity.Price,
+    //  PriceRange = bookEntity.PriceRange,
+
+    //  // Option(1) Get publishers related to book.
+    //  //Publishers = this.db.Publishers
+    //  //  .Where(publisherEntity => publisherEntity.Id == bookEntity.PublisherId)
+    //  //  .Select(publisherEntity => new Publisher
+    //  //  {
+    //  //    Id = publisherEntity.Id.ToString(),
+    //  //    Name = publisherEntity.Name,
+    //  //    Location = publisherEntity.Location
+    //  //  })
+    //  //  .ToList()
+
+    //  // Option(2) Avoid duplicate calls for the same publisher Id.
+    //  Publishers = this.db.Entry(bookEntity).References(u=>u.Publisher).Load(),
+    //});
+
+    // Option(3) Publishers are performed by one query together with Books. (Eager Loading)
+    List<BookEntity> bookEntities = await this.db.Books.Include(u=>u.Publisher).ToListAsync();
+    
+    var books = bookEntities.Select(bookEntity => new Book
+    {
+      Id = bookEntity.Id.ToString(),
+      Title = bookEntity.Title,
+      Author = bookEntity.Author,
+      Isbn = bookEntity.Isbn,
+      Price = bookEntity.Price,
+      PriceRange = bookEntity.PriceRange,
+      Publishers = bookEntities.Select(entry =>  new Publisher
+      {
+        Name = entry.Publisher.Name,
+        Location = entry.Publisher.Location,
+      }).ToList()
+     }).ToList();
+
+    return this.Ok(books);
+  }
+
+  [HttpGet(Name = "GetById")]
+  [ProducesResponseType(typeof(BookEntity), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public async Task<IActionResult> GetById(string? id)
+  {
+    if (string.IsNullOrEmpty(id))
+    {
+      return this.BadRequest();
+    }
+
+    BookEntity bookEntity = await this.db.Books.FirstOrDefaultAsync(book => book.Id == Guid.Parse(id));
+    Book bookResource = new Book
+    {
+      Id = bookEntity.Id.ToString(),
+      Author = bookEntity.Author,
+      Isbn = bookEntity.Isbn,
+      Price = bookEntity.Price,
+      PriceRange = bookEntity.PriceRange,
+      Title = bookEntity.Title,
+
+      //Publishers = this.db.Publishers
+      //  .Where(publisherEntity => publisherEntity.Id == bookEntity.PublisherId)
+      //  .Select(publisherEntity => new Publisher
+      //  {
+      //    Id = publisherEntity.Id.ToString(),
+      //    Name = publisherEntity.Name,
+      //    Location = publisherEntity.Location
+      //  })
+      //  .ToList(),
+
+      // Avoid duplicate calls for the same publisher Id.
+      Publishers = this.db.Entry(bookEntity).References(u => u.Publisher).Load(),
+    };
+
+    return bookResource == null ? this.NotFound() : this.Ok(bookResource);
+  }
+
+  [HttpPut(Name = "Update")]
+  [ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status409Conflict)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public async Task<IActionResult> Update(Book resource)
+  {
+    if (string.IsNullOrEmpty(resource.Id))
+    {
+      return this.BadRequest();
+    }
+
+    var BookEntity = new BookEntity
+    {
+      Id = Guid.Parse(resource.Id),
+      Title = resource.Title,
+      Author = resource.Author,
+      Isbn = resource.Isbn,
+      Price = resource.Price,
+      PriceRange = resource.PriceRange,
+    };
+
+    this.db.Books.Update(BookEntity);
+    await this.db.SaveChangesAsync();
+    return this.Ok();
+  }
+
+  [HttpDelete(Name = "Delete")]
+  [ProducesResponseType(typeof(BookEntity), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public async Task<IActionResult> Delete(string? id)
+  {
+    BookEntity BookEntity = new();
+    BookEntity = await this.db.Books.FirstOrDefaultAsync(entity => entity.Id == Guid.Parse(id));
+
+    if (BookEntity is null)
+    {
+      return this.NotFound();
+    }
+
+    this.db.Books.Remove(BookEntity);
+    await this.db.SaveChangesAsync();
+
+    return this.Ok();
+  }
+}
---
+++ EFCore/Controllers/BookDetailController.cs
@@ -0,0 +1,111 @@
+﻿namespace EFCore.Api.Controllers;
+
+using EFCore.Domain;
+using EFCore.Resources;
+using Infrastructure.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.IdentityModel.Tokens;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class BookDetailController : ControllerBase
+{
+  private readonly Repository db;
+
+  public BookDetailController(Repository db)
+  {
+    this.db = db;
+  }
+
+  [HttpGet(Name = "GetById")]
+  [ProducesResponseType(typeof(BookEntity), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public async Task<IActionResult> GetById(string id)
+  {
+    if (string.IsNullOrEmpty(id))
+    {
+      return this.BadRequest();
+    }
+    
+    BookEntity bookEntity = await this.db.Books.FirstOrDefaultAsync(u => u.Id == Guid.Parse(id));
+
+    if (bookEntity is null)
+    {
+      return this.NotFound();
+    }
+
+    // Explicit loading
+    //bookEntity.BookDetail = await this.db.BookDetails.FirstOrDefaultAsync(u => u.BookId == Guid.Parse(resource.Id));
+    bookEntity.BookDetail = await this.db.BookDetails.Include(u => u.Book).FirstOrDefaultAsync(u => u.BookId == Guid.Parse(id));
+
+    var bookDetailResource = new BookDetail
+    {
+      NumberOfPages = bookEntity.BookDetail.NumberOfPages,
+      NumberofChapters = bookEntity.BookDetail.NumberofChapters,
+      Weight = bookEntity.BookDetail.Weight,
+    };
+
+    return this.Ok(bookDetailResource);
+  }
+
+  [HttpPost(Name = "Create")]
+  [ProducesResponseType(typeof(BookDetail), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+  public async Task<IActionResult> Create(BookDetail resource)
+  {
+    if (resource.Id is null)
+    {
+      return this.BadRequest();
+    }
+    
+    var bookEntity = new BookEntity
+    {
+      BookDetail = new BookDetailEntity
+      {
+        NumberOfPages = resource.NumberOfPages,
+        NumberofChapters = resource.NumberofChapters,
+        Weight = resource.Weight,
+        BookId = Guid.Parse(resource.Id),
+      }
+    };
+
+    await this.db.Books.AddAsync(bookEntity);
+    await this.db.SaveChangesAsync();
+
+    return this.Ok();
+  }
+
+  [HttpPut(Name = "Update")]
+  [ProducesResponseType(typeof(BookDetail), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  [ProducesResponseType(StatusCodes.Status409Conflict)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public async Task<IActionResult> Update(BookDetail resource)
+  {
+    if (string.IsNullOrEmpty(resource.Id))
+    {
+      return this.BadRequest();
+    }
+
+    var bookDetail = new BookDetailEntity
+    {
+      NumberOfPages = resource.NumberOfPages,
+      NumberofChapters = resource.NumberofChapters,
+      Weight = resource.Weight,
+      BookId = Guid.Parse(resource.Id),
+    };
+
+    this.db.BookDetails.Update(bookDetail);
+    await this.db.SaveChangesAsync();
+
+    return this.Ok();
+  }
+}
